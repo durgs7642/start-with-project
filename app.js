be15dd5ef40d35763da1f61ad5297043b622744d
Durgesh Gupta <durgs7642@gmail.com>
2026-01-17 19:31:06 +0530

    remove errors

--- app.js
+++ app.js
@@ -85,14 +85,20 @@ passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
+app.use((req, res, next) => {
+  res.locals.currUser = req.user || null;
+  next();
+});
 
 app.use((req, res, next) => {
     res.locals.success = req.flash("success");
     res.locals.error = req.flash("error");
     console.log(res.locals.success);
-    res.locals.currUser = req.user || null;
+    res.locals.currUser = req.user ;
     next(); 
 });
+
+
 
 // app.get("/demouser", async(req, res) => {
 //    let fakeUser = new User({
